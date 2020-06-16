65ab9e8147046f2349020dad70969e659d6f210f
Mike <sigachev.m@gmail.com>
2020-06-16 18:37:52 -0400

    June 16 Async Validation on Reg Form!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   myDomain: 'zource1.us-east-1.elasticbeanstalk.com',
-  myPort: 80,
+  myPort: 5000,
   apiUrl: 'http://zource1.us-east-1.elasticbeanstalk.com/api',
   s3Bucket: 'https://zource-bucket.s3.us-east-1.amazonaws.com/',
 };
